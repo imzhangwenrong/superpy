19e554e6e8f7b9968a4e7defd9695282d4c441f4
emin.martinian <emin.martinian@f54bac50-a9c4-11de-ba6e-69e41c8db662>
2009-09-25 12:04:39 +0000

    (EM) Added stuff to make using easy install easier. Also added long_description to setup.py.

--- superpy/setup.py
+++ superpy/setup.py
@@ -1,6 +1,9 @@
 """Setup script for installing/distribuging package.
 """
 
+from misc.ez_setup import use_setuptools
+use_setuptools()
+
 
 from setuptools import setup, find_packages
 setup(
@@ -19,6 +22,31 @@ setup(
     requires = ['Pmw'],
     provides = ['superpy'],
     url = "http://code.google.com/p/superpy/",   # project home page
+    long_description = """
+Superpy distributes python programs across a cluster of machines or
+across multiple processors on a single machine. This is a
+coarse-grained form of parallelism in the sense that remote tasks
+generally run in separate processes and do not share memory with the
+caller.
+
+Key features of superpy include:
+
+    * Send tasks to remote servers or to same machine via XML RPC call
+    * GUI to launch, monitor, and kill remote tasks
+    * GUI can automatically launch tasks every day, hour, etc.
+    * Works on the Microsoft Windows operating system
+          o Can run as a windows service
+          o Jobs submitted to windows can run as submitting user
+    * Inputs/outputs are python objects via python pickle
+    * Pure python implementation
+    * Supports simple load-balancing to send tasks to best servers 
+
+What makes superpy different than the many other excellent parallel
+processing packages already available for python? The superpy package
+is designed to allow sending jobs across a large number of machines
+(both Windows and LINUX). This requires the ability to monitor, debug,
+and otherwise get information about the status of jobs.
+"""
     
     # could also include long_description, download_url, classifiers, etc.
 )
