08f35e07c4ea41fe3191eb5aaa2b502ef347870d
emin.martinian <emin.martinian@f54bac50-a9c4-11de-ba6e-69e41c8db662>
2009-10-29 16:44:40 +0000

    (EM)	Updated version since we are in dev.

--- trunk/superpy/setup.py
+++ trunk/superpy/setup.py
@@ -9,7 +9,7 @@ setup(
     name = "superpy",
     #version = "1.1.0.dev" would be a pre-release tag of dev,
     #version = "1.1.0-p1" would be a post-release tag of -p1,    
-    version = "1.1.0",
+    version = "1.2.0.dev",
     packages = find_packages(),
     test_suite = 'superpyTesting.unitTestsFromSrc.MakeMainSuperpyDoctest',
     scripts = ['superpy/scripts/%s' %s for s in [
