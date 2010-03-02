c4f1ee6f0576eff78cde81007a1e9a82452238fe
emin.martinian <emin.martinian@f54bac50-a9c4-11de-ba6e-69e41c8db662>
2010-03-02 22:25:25 +0000

    (EM) Fixed logging to make stdout and stderr go to rotating log file as well instead of having them go to their own files.

--- trunk/superpy/superpy/core/config.py
+++ trunk/superpy/superpy/core/config.py
@@ -8,11 +8,5 @@ This module should define the following configuration variables:
 
 smtpServer = 'FIXME'
 
-# Location that superpy service will use for stdout
-serviceStdoutFile = 'c:/temp/superpy_service_log.txt'
-
-# Location that superpy service will use for stderr
-serviceStderrFile = 'c:/temp/superpy_service_err.txt'
-
 # Location that superpy service will use for its logging
 serviceLogFile = 'c:/temp/superpy_log.txt'
